e567cd9dd828b3a5a9160234ef436d7388262f3d
Christie3D <145722275+Christie3D@users.noreply.github.com>
2023-09-21 18:35:43 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,81 @@
+window.requestAnimationFrame =
+	window.requestAnimationFrame ||
+	window.mozRequestAnimationFrame ||
+	window.webkitRequestAnimationFrame ||
+	window.msRequestAnimationFrame ||
+	window.oRequestAnimationFrame ||
+	function(callback) {
+	setTimeout(function() {
+		callback(Date.now());
+	}, 1000 / 60);
+};
+
+
+
+
+function setUpMouseHander(element, mouseDownFunc, mouseDragFunc, mouseUpFunc) {
+    /*
+           element -- either the element itself or a string with the id of the element
+           mouseDownFunc(x,y,evt) -- should return a boolean to indicate whether to start a drag operation
+           mouseDragFunc(x,y,evt,prevX,prevY,startX,startY)
+           mouseUpFunc(x,y,evt,prevX,prevY,startX,startY)
+       */
+    if (!element || !mouseDownFunc || !(typeof mouseDownFunc == "function")) {
+        throw "Illegal arguments in setUpMouseHander";
+    }
+    if (typeof element == "string") {
+        element = document.getElementById(element);
+    }
+    if (!element || !element.addEventListener) {
+        throw "first argument in setUpMouseHander is not a valid element";
+    }
+    var dragging = false;
+    var startX, startY;
+    var prevX, prevY;
+    
+    function doMouseDown(evt) {
+        if (dragging) {
+            return;
+        }
+        var r = element.getBoundingClientRect();
+        var x = evt.clientX - r.left;
+        var y = evt.clientY - r.top;
+        prevX = startX = x;
+        prevY = startY = y;
+        dragging = mouseDownFunc(x, y, evt);
+        if (dragging) {
+            document.addEventListener("mousemove", doMouseMove);
+            document.addEventListener("mouseup", doMouseUp);
+        }
+    }
+    
+    function doMouseMove(evt) {
+        if (dragging) {
+            if (mouseDragFunc) {
+                var r = element.getBoundingClientRect();
+                var x = evt.clientX - r.left;
+                var y = evt.clientY - r.top;
+                mouseDragFunc(x, y, evt, prevX, prevY, startX, startY);
+            }
+            prevX = x;
+            prevY = y;
+        }
+        console.log("Sub, MouseMove")
+    }
+    
+    function doMouseUp(evt) {
+        if (dragging) {
+            document.removeEventListener("mousemove", doMouseMove);
+            document.removeEventListener("mouseup", doMouseUp);
+            if (mouseUpFunc) {
+                var r = element.getBoundingClientRect();
+                var x = evt.clientX - r.left;
+                var y = evt.clientY - r.top;
+                mouseUpFunc(x, y, evt, prevX, prevY, startX, startY);
+            }
+            dragging = false;
+        }
+    }
+    element.addEventListener("mousedown", doMouseDown); }
+    
+    
